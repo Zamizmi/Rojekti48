27244fd3bed6f092c26df840636ec0e21b2eba3e
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2017-05-19 23:14:52 +0300

    initial files

--- lib/gameWindow.rb
+++ lib/gameWindow.rb
@@ -1,11 +1,52 @@
 require 'gosu'
 
-class GameWindow < Gosu::Window
+WIDTH, HEIGHT = 640, 480
+
+class GameWindow(width=WIDTH, height=HEIGHT) < Gosu::Window
   def initialize(*args)
 
+    super width, height
+
+    self.caption = "MoreGun"
+
+    @background = Gosu::Image.new("media/space.png", :tileable => true)
+    @level = Level.new("media/cptn_ruby_map.txt")
+    @character = Player.new(@level, 400, 100)
+    # The scrolling position is stored as top left corner of the screen.
+    @camera_x = @camera_y = 0
+  end
+
+  def update
+    move_x = 0
+    move_x -= 5 if Gosu.button_down? Gosu::KB_LEFT
+    move_x += 5 if Gosu.button_down? Gosu::KB_RIGHT
+    @character.update(move_x)
+    @character.collect_gems(@level.gems)
+    # Scrolling follows player
+    @camera_x = [[@character.x - WIDTH / 2, 0].max, @level.width * 50 - WIDTH].min
+    @camera_y = [[@character.y - HEIGHT / 2, 0].max, @level.height * 50 - HEIGHT].min
+  end
+
+  def draw
+    @background.draw 0, 0, 0
+    Gosu.translate(-@camera_x, -@camera_y) do
+      @level.draw
+      @character.draw
+    end
+  end
+
+  def button_down(id)
+    case id
+      when Gosu::KB_UP
+        @character.try_to_jump
+      when Gosu::KB_ESCAPE
+        close
+      else
+        super
+    end
   end
 end
 
 
-window = Gosu::Window.new(800,600, false)
-window.show
+window = GameWindow.new(WIDTH, HEIGHT)
+window.show if __FILE__ == $0
